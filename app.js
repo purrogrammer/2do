ff588a9cc9a105ca37530954c163bb96308f1442
Marguerite Kenedy <reach.marguerite@gmail.com>
2020-07-01 14:59:20 -0700

    add css and header footer templates

--- app.js
+++ app.js
@@ -4,32 +4,43 @@ const bodyParser = require("body-parser");
 const app = express(); 
 
 let items = []; 
+let newItems = []; 
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/", function(req, res) {
 
-    var today = new Date();
+   let today = new Date();
  
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
-
-
-    res.render("list", {kindOfDay: day, newListItems: items});
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
-app.post("/", (req, res) => {
-  
+app.post("/", function(req, res) {
+
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    if(req.body.list === "Work") { 
+       workItems.push(item);
+       res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    }
+  });
+
+app.get("/work", function (req, res) {
+    let listTitle = req.body.list.value;
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 app.listen(3000, function(){
